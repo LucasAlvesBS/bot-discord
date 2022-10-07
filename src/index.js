7663cd741e1e326245742864de45f555a7577405
Lucas Alves <lucasalvesbs6@gmail.com>
2022-10-07 09:35:59 -0300

    style: organize exports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
 const utils = require('./helpers/utils');
 const { discord_token } = require('./config/credentials');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
 
 client.login(discord_token);
 
